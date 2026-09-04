01100e496cbcbd246b44ab628358cd10ef34a166
Joshy <josiasrgz@gmail.com>
2026-09-03 22:33:35 -0600

    variables python

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print ("Hola mundo, Python ya funciona")
+nombre = "Joshy"
+edad = 25
+promedio = 9.5
+es_estudiante = True
+
+print (f"Hola {nombre}")
+print (f"Su edad es: {edad}")
+print (f"Eres estudiante: {es_estudiante}")
+
+
